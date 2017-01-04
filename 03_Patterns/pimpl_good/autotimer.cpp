5df10b6b61790884240dc176199456be20cc2f66
Tal Lancaster <tal@mckoy.local>
2017-01-03 19:36:15 -0700

    added 03_Patterns/pimpl_good

---
+++ 03_Patterns/pimpl_good/autotimer.cpp
@@ -0,0 +1,78 @@
+/// -*- tab-width: 4; c-basic-offset: 4; indent-tabs-mode: t -*-
+///
+/// \file   autotimer.cpp
+/// \author Martin Reddy
+/// \brief  An example of Pimpl with a smart pointer.
+///
+/// Copyright (c) 2010, Martin Reddy. All rights reserved.
+/// Distributed under the X11/MIT License. See LICENSE.txt.
+/// See http://APIBook.com/ for the latest version.
+///
+
+/**
+ - using brace-initializer
+ - replaced literals with constexpr.
+ - replaced new with make_unique: see Tweaks_Explained.md for reason
+ - added Impl constructor that takes a string so AutoTimer ctor can take advantage of it.
+ **/
+#include "autotimer.h"
+
+#include <iostream>
+
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <sys/time.h>
+#endif
+
+namespace apibook {
+
+class AutoTimer::Impl
+{
+public:
+    explicit Impl (const std::string& name) :
+    mName {name}
+    { }
+    
+	/// Return how long the object has been alive
+	double GetElapsed() const
+	{
+#ifdef _WIN32
+        constexpr auto Resolution {1e3};
+		return (GetTickCount() - mStartTime) / Resolution;
+#else
+        constexpr auto Resolution {1e6};
+		struct timeval end_time;
+		gettimeofday (&end_time, NULL);
+        double t1 {mStartTime.tv_usec / Resolution + mStartTime.tv_sec };
+        double t2 {end_time.tv_usec / Resolution + end_time.tv_sec };
+		return t2 - t1;
+#endif
+	}
+
+	std::string mName;
+#ifdef _WIN32
+	DWORD mStartTime;
+#else
+	struct timeval mStartTime;
+#endif
+};
+
+AutoTimer::AutoTimer(const std::string &name) :
+    mImpl {std::make_unique <Impl>(name) }
+{
+
+#ifdef _WIN32
+	mImpl->mStartTime = GetTickCount();
+#else
+	gettimeofday(&mImpl->mStartTime, NULL);
+#endif
+}
+
+AutoTimer::~AutoTimer()
+{
+	std::cout << mImpl->mName << ": took " << mImpl->GetElapsed()
+			  << " secs" << std::endl;
+}
+
+}
---
+++ 03_Patterns/pimpl_good/autotimer.h
@@ -0,0 +1,45 @@
+/// -*- tab-width: 4; c-basic-offset: 4; indent-tabs-mode: t -*-
+///
+/// \file   autotimer.h
+/// \author Martin Reddy
+/// \brief  An example of Pimpl with a smart pointer.
+///
+/// Copyright (c) 2010, Martin Reddy. All rights reserved.
+/// Distributed under the X11/MIT License. See LICENSE.txt.
+/// See http://APIBook.com/ for the latest version.
+///
+
+/**
+ This is essentially the impll_boost example but using c++11 unique_ptr smart ptr
+ - dropped boost::scoped_ptr for std::unique_ptr
+ **/
+
+#ifndef AUTOTIMER_H
+#define AUTOTIMER_H
+
+#include <memory>
+#include <string>
+
+
+namespace apibook {
+
+///
+/// An object that reports how long it was alive for when it
+/// is destroyed.
+///
+class AutoTimer
+{
+public:
+	/// Create a new timer object with a human-readable name
+	explicit AutoTimer(const std::string &name);
+	/// On destruction, the timer reports how long it was alive
+	~AutoTimer();
+
+private:
+    class Impl;
+	std::unique_ptr<Impl> mImpl;
+};
+
+}
+
+#endif
